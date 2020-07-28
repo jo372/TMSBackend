610bb3ba2e8c9698196183aacd5420cd4029a1e1
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-07-28 19:41:00 +0100

    removing custom logger and using winston.js

--- app.js
+++ app.js
@@ -6,7 +6,67 @@ const app = express();
 const sqlite3 = require('sqlite3').verbose();
 const validate = require('validate.js');
 const Database = require('./lib/Database');
-//const Logger = require('./lib/Logger');
+const winston = require('winston');
+const env = require('./env.js');
+
+function getTodaysDate() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth() + 1;
+    let yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    return `${dd}-${mm}-${yyyy}`;
+}
+
+// setting up my logger for all modules.
+winston.loggers.add(env.DEFAULT_LOGGER_NAME, {
+    level: 'debug',
+    transports : [
+        new winston.transports.Console(),
+        new winston.transports.File({filename:  `logs/${getTodaysDate()}-combined.log`})
+    ]
+});
+
+// getting the current logger.
+const logger = winston.loggers.get(env.DEFAULT_LOGGER_NAME);
+
+
+// get users tasks by id
+app.get('/users/:userid/tasks/:taskid', function(req, res) {
+    let userId = req.params.userid;
+    let taskId = req.params.taskid;
+    if(userId && taskId) {
+        res.send(`userId: ${userId}\ntaskId: ${taskId}`);
+        //TODO: check that the requesting user is the current logging in user.
+    }
+});
+
+// get all of users tasks
+app.get('/users/:userid/tasks', function(req, res) {
+    const query = req.query;
+    // if there is a query, check if it's a filter.
+    if(query) {
+        const parentTaskId = query.parentid;
+        res.send({
+            'test': 1,
+            'test2': 2
+        })
+    } else {
+        // return defaults.
+    }
+})
+
+
+let listener = app.listen(env.APPLICATION_LISTENING_PORT, function() {
+    console.log(`Listening on port ${listener.address().port}`);
+});
+
+
 
 /*
 var dbConnection = null;
@@ -48,38 +108,9 @@ function setupTables(conn) {
     setupTables(conn);
 
     if (conn) {
-        
+
         conn.close();
 
     }
 
 })();*/
-
-const serverPort = 8888;
-
-// get users tasks by id
-app.get('/users/:userid/tasks/:taskid', function(req, res) {
-    let userId = req.params.userid;
-    let taskId = req.params.taskid;
-    if(userId && taskId) {
-        res.send(`userId: ${userId}\ntaskId: ${taskId}`);
-        //TODO: check that the requesting user is the current logging in user.
-    }
-});
-
-// get all of users tasks
-app.get('/users/:userid/tasks', function(req, res) {
-    let query = req.query;
-    // if there is a query, check if it's a filter.
-    if(query) {
-        let parentTaskId = query.parentid;
-        console.log(query);
-    } else {
-        // return defaults.
-    }
-})
-
-
-let listener = app.listen(serverPort, function() {
-    console.log(`Listening on port ${listener.address().port}`);
-});
