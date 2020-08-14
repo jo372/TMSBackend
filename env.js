704a9a92ebb79281743fe8dea2bb09654486eb99
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-08-14 23:49:25 +0100

    adding database path and promise timeouts (may become deprecated in the future)

--- env.js
+++ env.js
@@ -1,6 +1,8 @@
 class env {
     static DEFAULT_LOGGER_NAME = 'tms-logger';
     static APPLICATION_LISTENING_PORT = 8888;
+    static DATABASE_PATH = 'db/test.db';
+    static PROMISE_TIMEOUT = 1000;
 }
 
 Object.freeze(env);
