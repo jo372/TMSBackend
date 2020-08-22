f931a66aecd98afc6af29c53e2fc3f915e5859e3
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-08-22 16:48:12 +0100

    refactoring the database test file to use typescript.

---
+++ test/DatabaseTest.ts
@@ -0,0 +1,14 @@
+import {
+    expect,
+    assert,
+    should
+} from 'chai';
+import 'mocha';
+import {Database} from "../src/Database";
+
+const WORKING_DB_DIR : string = './db/test.db';
+
+// TODO: rewrite the database tests.
+describe('constructor', () => {
+
+});
