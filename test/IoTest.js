84cabbd67da1904f7e5ea07dfc47d8e035aa143c
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-08-22 16:48:48 +0100

    created an IoTest class with typescript

---
+++ test/IoTest.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const assert = require('assert');
+const should = chai.should();
+const expect = chai.expect;
+const io = require('../lib/Io');
+
+describe('fileExists', function() {
+    expect(function() {
+        io.fileExists('./db/non-existent-database.db');
+    }).to.throw(Error);
+});
