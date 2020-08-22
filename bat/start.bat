afa9b4bf9f8dc634f65464a77d718f6ee42b8547
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-08-22 20:36:05 +0100

    create a batch file which allows us to start pm2

--- bat/start.bat
+++ bat/start.bat
@@ -1,2 +1,11 @@
 @echo off
-cd ../ && node start
+
+if "%ENV_BAT_PATH%"=="" (
+    set ENV_BAT_PATH="./env.bat"
+)
+
+call %ENV_BAT_PATH% get_application_name
+call %ENV_BAT_PATH% get_start_script_name
+
+cd ../
+pm2 start %START_SCRIPT_NAME% --name %APPLICATION_NAME% -- start & cd "./bat/"
