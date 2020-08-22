de778b6f24596a101db98064a6893ee9e6550604
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-08-22 16:47:32 +0100

    refactored the original app.js and moved it into the approriate folder

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,36 @@
-import Express from 'express';
+import express from "express";
+import {Logger} from "./Logger";
+import {Database} from './Database';
+import {Env} from './env';
 
-const app = Express();
+const app = express();
+const port : number = 8080;
+
+app.get('/users/:userid/tasks/:taskid', (req : any, res : any) => {
+    const userId : number = parseInt(req.params.userid, 10);
+    const taskId : number = parseInt(req.params.taskid, 10);
+
+    if(userId && taskId) {
+        res.send(`userId: ${userId}\ntaskId:${taskId}`);
+        Logger.info(`Requested UserId:${userId} & TaskId: ${taskId}`);
+    }
+});
+
+// get all of users tasks
+app.get('/users/:userid/tasks', (req: any, res : any)  => {
+    const query : any = req.query;
+    // if there is a query, check if it's a filter.
+    if(query) {
+        const parentTaskId = query.parentid;
+        res.send({
+            'test': 1,
+            'test2': 2
+        })
+    } else {
+        // return defaults.
+    }
+});
+
+app.listen(Env.APPLICATION_LISTENING_PORT, () => {
+   Logger.info(`Listening on port ${Env.APPLICATION_LISTENING_PORT}`);
+});
