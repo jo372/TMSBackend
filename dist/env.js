1b6e70d0a47a7e739aa6f2539f3019c429e83543
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-08-23 11:05:58 +0100

    removing all the created files from typescript.

--- dist/env.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Env = void 0;
-class Env {
-}
-exports.Env = Env;
-Env.DEFAULT_LOGGER_NAME = 'tms-logger';
-Env.APPLICATION_LISTENING_PORT = 8888;
